83f46a651d3b9d5f0e352e50f26b761da0189e7f
0ahz <shuangoahz@gmail.com>
2024-02-26 16:31:23 +0800

    chore: output file

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,8 +7,8 @@ export default defineConfig(options => {
     target: ['es2020', 'node16', 'chrome60', 'firefox60', 'safari11', 'edge18'],
     entry: {
       index: 'src/index.ts',
-      pushover: 'src/pushover/index.ts',
-      bark: 'src/bark/index.ts',
+      'pushover/index': 'src/pushover/index.ts',
+      'bark/index': 'src/bark/index.ts',
     },
     outDir: 'dist',
     format: ['esm', 'cjs'],
