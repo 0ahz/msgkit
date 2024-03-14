a74d18724c0f7b35b43b4750613a8676aa629eaa
0ahz <shuangoahz@gmail.com>
2024-03-14 22:37:37 +0800

    chore: "moduleResolution": "bundler"

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from 'tsup'
 
-// https://tsup.egoist.sh/#using-custom-configuration
-// https://paka.dev/npm/tsup/v/5.12.4#module-index-export-Options
 export default defineConfig(options => {
   return {
-    target: ['es2020', 'node16', 'chrome60', 'firefox60', 'safari11', 'edge18'],
+    target: ['es2020'],
     entry: {
       index: 'src/index.ts',
       'pushover/index': 'src/pushover/index.ts',
       'bark/index': 'src/bark/index.ts',
     },
     outDir: 'dist',
-    format: ['esm', 'cjs'],
-    dts: {
-      resolve: true,
-    },
+    format: ['cjs', 'esm'],
+    dts: true,
     bundle: true,
     splitting: true,
     treeshake: true,
